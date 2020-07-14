94be0d2de7eae4cce5619697813853a00369351b
Pranay Jay Patel <pjpatel1012@gmail.com>
2020-07-13 20:10:53 -0400

    Initial Commit

---
+++ SafeTravels/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SafeTravels
+//
+//  Created by Pranay Jay Patel on 7/13/20.
+//  Copyright © 2020 Pranay Jay Patel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SafeTravels/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SafeTravels
+//
+//  Created by Pranay Jay Patel on 7/13/20.
+//  Copyright © 2020 Pranay Jay Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
